fad3454ec3bc9fc117900397c266d6d470a25e50
imanoldam1B <iperezadam1B@gmail.com>
2023-10-28 18:08:21 +0200

    Primera Window hecha, en curso la segunda

--- Navegación de Imanol/Window1.xaml.cs
+++ Navegación de Imanol/Window1.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Navegación_de_Imanol
 {
     /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
+    /// Lógica de interacción para Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void Boton1_MainWindow_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
