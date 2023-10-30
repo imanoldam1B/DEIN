d9c8ac52b846e187283ff45d36f3344bf9037c18
Alberto <agonzalber10@educacion.navarra.es>
2023-10-30 12:43:02 +0100

    Corregido el error que no te lanzaba la page1

--- Navegación de Imanol/Page1.xaml.cs
+++ Navegación de Imanol/Page1.xaml.cs
@@ -17,7 +17,7 @@ namespace Navegación_de_Imanol
     /// <summary>
     /// Lógica de interacción para Page1.xaml
     /// </summary>
-    public partial class Page1 : Window
+    public partial class Page1 : Page
     {
         public Page1()
         {
