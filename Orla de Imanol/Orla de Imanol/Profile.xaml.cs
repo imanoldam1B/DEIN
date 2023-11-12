033aedeccafc147f15b42bd15fc0d8fb485931b8
imanoldam1B <iperezadam1B@gmail.com>
2023-11-12 23:41:33 +0100

    Resultado pésimo
    No se como declarar nuevos puestos con sus atributos y que todo funcione bien. Estoy bloqueado y perdido. Entrego lo que había hecho en un principio (mal) por hacer acto de presencia

--- Orla de Imanol/Orla de Imanol/Profile.xaml.cs
+++ Orla de Imanol/Orla de Imanol/Profile.xaml.cs
@@ -25,9 +25,7 @@ namespace Orla_de_Imanol
             Profile_Nombre.Text = Nombre;
             Profile_Apellidos.Text = Apellidos;
             Profile_Email.Text = Email;
-            Profile_Image.Source = new BitmapImage(new
-
-            Uri(string.Format(@"..\..\{0}", Foto), UriKind.Relative));
+            Profile_Image.Source = new BitmapImage(new Uri(string.Format(@"..\..\{0}", Foto), UriKind.Relative));
         }
     }
 }
