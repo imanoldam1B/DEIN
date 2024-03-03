85385ad2427b77f23cca614b7fe16cbb23888c12
imanoldam1B <iperezadam1B@gmail.com>
2024-03-03 19:03:02 +0100

    .

--- Formulario-NoUsable/Properties/AssemblyInfo.cs
+++ Formulario-NoUsable/Properties/AssemblyInfo.cs
@@ -1,12 +1,7 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// La información general de un ensamblado se controla mediante el siguiente 
-// conjunto de atributos. Cambie estos valores de atributo para modificar la información
-// asociada con un ensamblado.
 [assembly: AssemblyTitle("Introducción de Datos de Empleados")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
